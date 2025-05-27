81dee12e01d1f6016002b787943d21cc911698ea
Artur <82472015+benyaar@users.noreply.github.com>
2025-05-27 14:43:29 +0300

    Client-side Data Fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,41 @@
+'use client';
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+    const [isLoading, setIsLoading] = useState()
+    const [error, setError] = useState()
+    const [news, setNews] = useState()
+    useEffect(() => {
+        async function fetchNews(params) {
+            console.log(1);
+            
+            setIsLoading(true)
+            const response = await fetch('http://localhost:8080/news')
+
+            if(!response.ok){
+                setError('Failed to fetch news')
+                setIsLoading(false)
+            }
+
+            const news = await response.json()
+            setIsLoading(false)
+            setNews(news)
+        }
+        fetchNews()
+    }, [])
+
+    if(isLoading){
+        return <p>..Loading</p>
+    }
+    if(error){
+        return <p>{error}</p>
+    }
+
     return (
         <>
             <h1>NewsPage</h1>
-            <NewsList news={DUMMY_NEWS} />
+           {news && <NewsList news={news}/> }
         </>
     )
 }

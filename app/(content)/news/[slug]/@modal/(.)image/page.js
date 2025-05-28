bfa62e1b1846283d93e6f97002b7cc09847ccc75
Artur <82472015+benyaar@users.noreply.github.com>
2025-05-28 23:36:00 +0300

    add sql database

--- app/(content)/news/[slug]/@modal/(.)image/page.js
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,18 +1,19 @@
-'use client'
-import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound, useRouter } from "next/navigation";
 
-export default function InterceptedImagePage({ params }) {
-    const router = useRouter()
+import { getNewsItem } from "@/lib/news";
+import { notFound } from "next/navigation";
+
+
+export default async function InterceptedImagePage({ params }) {
+    
     const newsItemSlug = params.slug;
-    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug)
+    const newsItem = await getNewsItem(newsItemSlug)
 
     if (!newsItem) return (
         notFound()
     )
     return (
         <>
-            <div className="modal-backdrop" onClick={router.back}></div>
+            <ModalBackdrop/>
             <dialog className="modal" open>
                 <div className="fullscreen-image">
                     <img src={`/images/news/${newsItem.image}`} alt={newsItem.item} style={{
--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,41 +1,15 @@
-'use client';
+
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
+import { getAllNews } from "@/lib/news";
 
-export default function NewsPage() {
-    const [isLoading, setIsLoading] = useState()
-    const [error, setError] = useState()
-    const [news, setNews] = useState()
-    useEffect(() => {
-        async function fetchNews(params) {
-            console.log(1);
-            
-            setIsLoading(true)
-            const response = await fetch('http://localhost:8080/news')
+export default async function NewsPage() {
 
-            if(!response.ok){
-                setError('Failed to fetch news')
-                setIsLoading(false)
-            }
-
-            const news = await response.json()
-            setIsLoading(false)
-            setNews(news)
-        }
-        fetchNews()
-    }, [])
-
-    if(isLoading){
-        return <p>..Loading</p>
-    }
-    if(error){
-        return <p>{error}</p>
-    }
+    const news = await getAllNews()  
 
     return (
         <>
             <h1>NewsPage</h1>
-           {news && <NewsList news={news}/> }
+            <NewsList news={news}/> 
         </>
     )
 }
